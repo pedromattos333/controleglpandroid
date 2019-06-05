9941c61ac92c58ba97dc9ef7094644c171d16bbe
ControleGlp <pedro.neto@cursos.sc.senac.br>
2019-06-04 21:35:40 -0300

    test

---
+++ app/src/main/java/com/senac/pedro/controleglp/view/MovimentacaoActivity.java
@@ -0,0 +1,14 @@
+package com.senac.pedro.controleglp.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.senac.pedro.controleglp.R;
+
+public class MovimentacaoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movimentacao);
+    }
+}
---
+++ app/src/main/java/com/senac/pedro/controleglp/view/PrincipalActivity.java
@@ -0,0 +1,15 @@
+package com.senac.pedro.controleglp.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.senac.pedro.controleglp.R;
+
+public class PrincipalActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
